2e0b3f315b8bb92b475fb27dcf452e7f6d1db533
Neo-Techies <mbcse50@gmail.com>
2020-09-13 14:13:14 +0530

    Deployment Config

--- server.js
+++ server.js
@@ -153,5 +153,5 @@ app.get("/getlikecount/:address", (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server started at port at port ", process.env.PORT || 5000);
+  console.log("Server started at port ", process.env.PORT || 5000);
 });
